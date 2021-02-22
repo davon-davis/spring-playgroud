13bedd04c1272191a70f11839ba16add6c6980da
Davon Davis <davon.davis@austincc.edu>
2021-02-22 16:06:15 -0600

    first commit

---
+++ src/test/java/com/example/demo/HelloControllerTest.java
@@ -0,0 +1,31 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HelloController.class)
+public class HelloControllerTest {
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    public void testHomaPageEndpoint() throws Exception{
+        //localhost:8080
+        this.mvc.perform(get("/").accept(MediaType.TEXT_PLAIN))
+            .andExpect(status().isOk())//200
+            .andExpect(content().string("Hello from Spring!"));
+    }
+
+
+
+}
